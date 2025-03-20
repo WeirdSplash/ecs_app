c09a5d1c1e0b22326eba80e8ec5cbd615ef35975
Joice Kelly Chavez Vera <joice@Joices-MacBook-Air.local>
2025-03-20 13:59:53 -0500

    ecs app con ec2

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-state"
+    bucket         = "my-terraform-state-jk"
     key            = "ecs-app/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-lock"
