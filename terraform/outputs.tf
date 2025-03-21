16438cf1ff22a338f2ae43a9cc8eb4c528443d03
Joice Kelly Chavez Vera <joice@Joices-MacBook-Air.local>
2025-03-20 20:54:25 -0500

    Lanzando workflow de Terraform y Deploy

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,7 +2,7 @@ output "ecs_cluster_name" {
   value = aws_ecs_cluster.app_cluster.name
 }
 output "ecs_service_name" {
-  value = aws_ecs_service.app_cluster.name
+  value = aws_ecs_service.app_service.name
 }
 
 
