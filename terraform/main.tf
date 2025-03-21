b797bceb594e8cbc8bd43ace0b84c2989541b639
Joice Kelly Chavez Vera <joice@Joices-MacBook-Air.local>
2025-03-20 21:15:57 -0500

    Lanzando workflow de Terraform y Deploy

--- terraform/main.tf
+++ terraform/main.tf
@@ -120,3 +120,25 @@ resource "aws_lb_listener" "http" {
     target_group_arn = aws_lb_target_group.app_tg.arn
   }
 }
+
+resource "aws_ecs_service" "app_service" {
+  name            = "app-service"
+  cluster         = aws_ecs_cluster.app_cluster.id
+  task_definition = aws_ecs_task_definition.app_task.arn
+  desired_count   = 1
+  launch_type     = "EC2"
+  load_balancer {
+    target_group_arn = aws_lb_target_group.app_tg.arn
+    container_name   = "app-container"
+    container_port   = 8080
+  }
+}
+
+resource "aws_key_pair" "ecs_key" {
+  key_name   = "ecs-key"
+  public_key = "your-public-key-here"
+}
+
+resource "aws_ecr_repository" "app_repo" {
+  name = "app-repo"
+}
