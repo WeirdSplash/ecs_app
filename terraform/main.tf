c1409829d22ace780400f0c5c28d897129e0c0f7
Joice Kelly Chavez Vera <joice@Joices-MacBook-Air.local>
2025-03-20 21:05:50 -0500

    Lanzando workflow de Terraform y Deploy

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,44 +2,84 @@ provider "aws" {
   region = var.aws_region
 }
 
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "ecr_image_url" {
+  description = "URL de la imagen en ECR"
+  type        = string
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "main-vpc"
+  }
+}
+
+resource "aws_subnet" "public_1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "public_2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_security_group" "ecs_sg" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_iam_role" "ecs_execution_role" {
+  name = "ecsExecutionRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_execution_role_policy" {
+  role       = aws_iam_role.ecs_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
 resource "aws_ecs_cluster" "app_cluster" {
   name = "app-cluster"
-}
-
-resource "aws_launch_template" "ecs_launch_template" {
-  name_prefix            = "ecs-template-"
-  image_id               = "ami-0c55b159cbfafe1f0"
-  instance_type          = "t3.micro"
-  key_name               = aws_key_pair.ecs_key.key_name
-  vpc_security_group_ids = [aws_security_group.ecs_sg.id]
-
-  user_data = base64encode(<<EOF
-#!/bin/bash
-echo "ECS_CLUSTER=${aws_ecs_cluster.app_cluster.name}" >> /etc/ecs/ecs.config
-EOF
-  )
-}
-
-resource "aws_autoscaling_group" "ecs_asg" {
-  vpc_zone_identifier = [aws_subnet.public_1.id, aws_subnet.public_2.id]
-  desired_capacity    = 2
-  min_size            = 1
-  max_size            = 3
-  health_check_type   = "EC2"
-  health_check_grace_period = 300
-
-  launch_template {
-    id      = aws_launch_template.ecs_launch_template.id
-    version = "$${aws_launch_template.ecs_launch_template.latest_version}"
-  }
-}
-
-resource "aws_ecr_repository" "app_repo" {
-  name = "app-repo"
-
-  image_scanning_configuration {
-    scan_on_push = true
-  }
 }
 
 resource "aws_ecs_task_definition" "app_task" {
@@ -47,7 +87,6 @@ resource "aws_ecs_task_definition" "app_task" {
   network_mode             = "bridge"
   requires_compatibilities = ["EC2"]
   execution_role_arn       = aws_iam_role.ecs_execution_role.arn
-  task_role_arn            = aws_iam_role.ecs_task_role.arn
   cpu                      = "256"
   memory                   = "512"
 
@@ -62,18 +101,6 @@ resource "aws_ecs_task_definition" "app_task" {
       hostPort      = 8080
     }]
   }])
-}
-
-resource "aws_ecs_service" "app_service" {
-  name            = "app-service"
-  cluster         = aws_ecs_cluster.app_cluster.id
-  task_definition = aws_ecs_task_definition.app_task.arn
-  desired_count   = 2
-
-  capacity_provider_strategy {
-    capacity_provider = "EC2CapacityProvider"
-    weight            = 1
-  }
 }
 
 resource "aws_lb" "app_lb" {
@@ -100,75 +127,5 @@ resource "aws_lb_listener" "http" {
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.app_tg.arn
-  }
-}
-
-resource "aws_lb_listener" "https" {
-  load_balancer_arn = aws_lb.app_lb.arn
-  port              = 443
-  protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-2016-08"
-  certificate_arn   = "arn:aws:acm:REGION:ACCOUNT_ID:certificate/CERTIFICATE_ID"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.app_tg.arn
-  }
-}
-
-resource "aws_security_group" "ecs_sg" {
-  vpc_id = aws_vpc.main.id
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["TU_IP/32"]
-  }
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_appautoscaling_target" "ecs_target" {
-  max_capacity       = 3
-  min_capacity       = 1
-  resource_id        = "service/${aws_ecs_cluster.app_cluster.name}/${aws_ecs_service.app_service.name}"
-  scalable_dimension = "ecs:service:DesiredCount"
-  service_namespace  = "ecs"
-}
-
-resource "aws_appautoscaling_policy" "ecs_scaling_policy" {
-  name               = "ecs-scaling-policy"
-  policy_type        = "TargetTrackingScaling"
-  resource_id        = aws_appautoscaling_target.ecs_target.resource_id
-  scalable_dimension = aws_appautoscaling_target.ecs_target.scalable_dimension
-  service_namespace  = aws_appautoscaling_target.ecs_target.service_namespace
-
-  target_tracking_scaling_policy_configuration {
-    target_value       = 50
-    predefined_metric_specification {
-      predefined_metric_type = "ECSServiceAverageCPUUtilization"
-    }
-    scale_in_cooldown  = 60
-    scale_out_cooldown = 60
   }
 }
